e170c4e38cd281aedb92a22d2daa1ccbd14b2028
Hoàng Văn Nhân <hoangthienret@gmail.com>
2024-11-22 20:49:14 +0700

    Complete the topic: Classifier Member and Instance Member

--- Lab03/AimsProject/src/Entity/Aims.java
+++ Lab03/AimsProject/src/Entity/Aims.java
@@ -6,12 +6,17 @@ public class Aims {
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
         anOrder.addDigitalVideoDisc(dvd1);
+        System.out.println("DVD 1 ID: " + dvd1.getId());
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
         anOrder.addDigitalVideoDisc(dvd2);
+        System.out.println("DVD 2 ID: " + dvd2.getId());
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "John Musker", 90, 18.99f);
         anOrder.addDigitalVideoDisc(dvd3);
+        System.out.println("DVD 3 ID: " + dvd3.getId());
+
+        System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
 
         System.out.print("Total cost is: ");
         System.out.println(anOrder.totalCost());
